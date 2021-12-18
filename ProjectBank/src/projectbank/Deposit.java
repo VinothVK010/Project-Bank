4af84805252d06f6731d659af6f6d5b38ea50ab2
karthika41 <sjkarthika41@gmail.com>
2021-12-18 16:36:47 +0530

    added some code deposit

--- ProjectBank/src/projectbank/Deposit.java
+++ ProjectBank/src/projectbank/Deposit.java
@@ -1,10 +1,14 @@
 package projectbank;
 
+import java.util.Scanner;
+
 public class Deposit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s=new Scanner(System.in);
+        System.out.println("Enter your deposit amount:");
+        String depositamount=s.next();
+        
 	}
 
 }
